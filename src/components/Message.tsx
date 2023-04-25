0964314bc07f4f6aa04d1ceab5a7720a36b33af4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 21:37:10 +0200

    updated tailwind

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -7,16 +7,13 @@ const Message: React.FC = () => {
         <img
           src="https://ath2.unileverservices.com/wp-content/uploads/sites/4/2020/02/IG-annvmariv.jpg"
           alt="user image"
-          className="h-8 w-8 rounded-full object-fill"
+          className="m-auto h-8 w-8 rounded-full object-fill"
         />
         <p>Just now</p>
       </span>
       <span className="flex flex-col gap-1">
         <p className="flex flex-col justify-center rounded-b-lg rounded-r-lg bg-slate-600 p-1 text-left">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          voluptate minima culpa esse, repellat repudiandae voluptatem.
-          Consequuntur vero blanditiis quaerat cupiditate deleniti molestias,
-          consequatur beatae, aliquam reprehenderit tempore fugit eos?
+          Lorem ipsum
         </p>
         <img
           src="https://ath2.unileverservices.com/wp-content/uploads/sites/4/2020/02/IG-annvmariv.jpg"
