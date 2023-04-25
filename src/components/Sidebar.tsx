bfea943db473907aace9e90acc450fdbb494c7bb
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 10:52:25 +0200

    imported navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Sidebar: React.FC = () => {
-  return <div className="basis-1/3">Sidebar</div>;
+  return (
+    <div className="basis-1/3 border-r-2 bg-slate-500">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Sidebar;
