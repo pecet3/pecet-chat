0431a6567c7f6209a5ececc7d4396ae7cf6ec7ea
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 10:25:59 +0200

    edited home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Chat from ".././components/Chat";
+import Sidebar from ".././components/Sidebar";
 
 const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="m-auto flex h-[500px] max-w-[768px] justify-center gap-2 border-2 border-black bg-slate-500">
+      <Sidebar />
+      <Chat />
+    </div>
+  );
 };
 
 export default Home;
