7724db1d9f2d0432c53274063e359b07b1903536
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 19:53:22 +0200

    created connfig

---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsC0mV0AsdVkuebK4CPMmVtwkTSRQHPTU",
+  authDomain: "pecetchat.firebaseapp.com",
+  projectId: "pecetchat",
+  storageBucket: "pecetchat.appspot.com",
+  messagingSenderId: "570619898052",
+  appId: "1:570619898052:web:618ebbaa4b9b3173c50191",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
