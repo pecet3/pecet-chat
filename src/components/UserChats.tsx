63cbdbf9b2ffa5504052b06f0f0651b1755ba976
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 21:11:29 +0200

    updated tailwind

--- src/components/UserChats.tsx
+++ src/components/UserChats.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const UserChats: React.FC = () => {
   return (
-    <div className="flex items-center gap-1 border-2  text-left hover:bg-slate-400">
+    <div className="flex items-center gap-1 text-left duration-200 hover:bg-slate-600">
       <img
         src="https://d-art.ppstatic.pl/kadry/k/r/1/57/8f/6290b1787d225_o_large.jpg"
-        className="h-10 w-10 rounded-full object-cover"
+        className="ml-1 h-10 w-10 rounded-full object-cover"
       />
       <span>
         <p className="font-bold text-slate-200 ">Agnieszka</p>
