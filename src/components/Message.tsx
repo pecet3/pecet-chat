1361c487f721a783ec95151608b0f982839b9e7d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 15:14:13 +0200

    updated component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Message: React.FC = () => {
-  return <div className="flex text-slate-200">asdasdsaddasdas</div>;
+  return (
+    <div className="flex gap-1 text-slate-200">
+      <span>
+        <img
+          src="https://ath2.unileverservices.com/wp-content/uploads/sites/4/2020/02/IG-annvmariv.jpg"
+          alt="user image"
+          className="h-8 w-8 rounded-full object-fill"
+        />
+        <p>Just now</p>
+      </span>
+      <span className="flex flex-col gap-1">
+        <p className="flex flex-col justify-center rounded-b-lg rounded-r-lg bg-slate-600 p-1 text-left">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+          voluptate minima culpa esse, repellat repudiandae voluptatem.
+          Consequuntur vero blanditiis quaerat cupiditate deleniti molestias,
+          consequatur beatae, aliquam reprehenderit tempore fugit eos?
+        </p>
+        <img
+          src="https://ath2.unileverservices.com/wp-content/uploads/sites/4/2020/02/IG-annvmariv.jpg"
+          alt="photo which user sent"
+          className=" max-h-32 self-start"
+        />
+      </span>
+    </div>
+  );
 };
 
 export default Message;
