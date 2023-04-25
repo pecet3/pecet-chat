0a94062536aeb3c3feeb9b5aa881b5e14794b14b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 21:11:21 +0200

    updated tailwind

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 
 const Messages: React.FC = () => {
   return (
-    <div className="flex h-[calc(100%-112px)] flex-col gap-1 p-1">
+    <div className="flex h-[calc(100%-112px)] flex-col gap-1 overflow-y-scroll p-1">
       <Message /> <Message /> <Message /> <Message />
     </div>
   );
