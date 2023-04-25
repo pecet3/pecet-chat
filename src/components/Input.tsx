9ae0a6543317ba358534cd70185c8758c88e2fc1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 15:14:05 +0200

    edited input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { BiImageAdd } from "react-icons/bi";
+import { MdAttachFile } from "react-icons/md";
 
 const Input: React.FC = () => {
   return (
-    <div className="flex ">
+    <div className="flex gap-1 bg-gray-300 p-1">
       <input
         type="text"
         placeholder="enter your message"
-        className="w-full p-2"
+        className="w-full rounded-md p-1 text-left"
       />
+      <span className="flex items-center">
+        <BiImageAdd size="28" />
+        <input type="file" id="file" className="hidden" />
+        <label htmlFor="file">
+          <MdAttachFile size="28" />
+        </label>
+      </span>
+      <button className="submitButton">Send</button>
     </div>
   );
 };
