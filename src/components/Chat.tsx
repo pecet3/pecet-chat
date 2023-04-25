a64efb63a39d16e69c29153a8d875faacb557817
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 12:40:48 +0200

    updated component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BsCameraVideo, BsThreeDots, BsPersonAdd } from "react-icons/bs";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat: React.FC = () => {
-  return <div className="basis-2/3 bg-slate-400">chat</div>;
+  return (
+    <div className=" basis-2/3 flex-col justify-between bg-slate-400">
+      <div className="flex justify-between bg-slate-500 px-2 py-5 text-slate-200">
+        <p>Friend's Name</p>
+        <span className="flex gap-2">
+          <BsCameraVideo size="24" />
+          <BsThreeDots size="24" />
+          <BsPersonAdd size="24" />
+        </span>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 };
 
 export default Chat;
