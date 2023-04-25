776312efeb5f086f93d77de3011068e3ad1daf5d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 12:41:28 +0200

    implemented search and userchats

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Search from "./Search";
+import UserChats from "./UserChats";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="basis-1/3 border-r-2 bg-slate-500">
+    <div className="basis-1/3 bg-slate-500">
       <Navbar />
+      <Search />
+      <UserChats />
     </div>
   );
 };
