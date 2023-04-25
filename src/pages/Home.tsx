f860a5e01b47e958a051bfdb3699530e64e9a8fe
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-26 00:09:41 +0200

    updated tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Sidebar from ".././components/Sidebar";
 
 const Home: React.FC = () => {
   return (
-    <div className="m-auto flex h-[700px] max-w-[1000px] justify-center overflow-hidden rounded-xl">
+    <div className="m-auto flex h-[700px] max-w-[1000px] justify-center overflow-hidden rounded-xl shadow-xl shadow-slate-400">
       <Sidebar />
       <Chat />
     </div>
