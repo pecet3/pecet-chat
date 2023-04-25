ed82617ca69bf3332807e600a6226fa9a4199c74
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 19:53:35 +0200

    updated tailwind

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,7 +5,7 @@ import Input from "./Input";
 
 const Chat: React.FC = () => {
   return (
-    <div className=" basis-2/3 flex-col justify-between bg-slate-400">
+    <div className="basis-2/3 flex-col justify-between bg-slate-400">
       <div className="flex justify-between bg-slate-500 px-2 py-5 text-slate-200">
         <p>Friend's Name</p>
         <span className="flex gap-2">
