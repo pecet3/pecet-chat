29e94539663ce3ec52de5935c8cdc7cafe38ffa9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-25 10:52:04 +0200

    updated tailwind

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Chat: React.FC = () => {
-  return <div className="basis-2/3 border-2">chat</div>;
+  return <div className="basis-2/3 bg-slate-400">chat</div>;
 };
 
 export default Chat;
