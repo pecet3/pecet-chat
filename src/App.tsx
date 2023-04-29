67b47467e3451effd3e2878875be3926e5d2c7e1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-29 23:43:17 +0200

    created router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import "./App.css";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Home />
-      <Register />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
