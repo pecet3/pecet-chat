a4aef9053394fc933fc1d7367d85db2d06e4f7b3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-30 21:38:53 +0200

    added reqiurment to display span with img and userName

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,13 +10,15 @@ const Navbar: React.FC = () => {
       <nav className="flex flex-col bg-slate-700 p-2 text-gray-200">
         <h1 className="font-bold ">JakubChat</h1>
         <span className="flex justify-between">
-          <span className="flex gap-1">
-            <img
-              src={user?.photoURL ? user?.photoURL : ""}
-              className="h-6 w-6 rounded-full"
-            />
-            <p>{user?.displayName ? user.displayName : ""}</p>
-          </span>
+          {user && (
+            <span className="flex gap-1">
+              <img
+                src={user.photoURL ? user.photoURL : ""}
+                className="h-6 w-6 rounded-full"
+              />
+              <p>{user.displayName}</p>
+            </span>
+          )}
 
           <button
             className="rounded-md bg-slate-300 px-1 text-xs text-slate-900 transition-all duration-200 hover:rounded-lg"
