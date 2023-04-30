861f4075a86c08b2329336328a6f464e87702c57
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-30 19:36:30 +0200

    updated tailwind

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Search: React.FC = () => {
   return (
-    <div className="flex border-b border-b-gray-400 text-slate-200">
+    <div className="flex border-b border-b-gray-400 bg-cyan-600 ">
       <input
         type="text"
         placeholder="Find a user 🔎"
-        className="w-full bg-transparent p-1 pl-2"
+        className="w-full bg-transparent p-1 pl-2 text-slate-50 placeholder-gray-100"
       />
     </div>
   );
