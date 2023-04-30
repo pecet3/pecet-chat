95ac979699a191abe0a5f56ba3512295d8a67abb
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-30 15:03:18 +0200

    created protectedRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import React from "react";
 import "./App.css";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Context, { IContext } from "./AuthContext";
+
+interface IProtectedRoute {
+  children: React.ReactNode;
+}
+
+type TProtectedRouteReturn = React.ReactElement | null;
+
+const ProtectedRoute: React.FC<IProtectedRoute> = ({
+  children,
+}): TProtectedRouteReturn => {
+  const { user } = React.useContext(Context) as IContext;
+  if (!user) return <Navigate to="/login" replace />;
+  return children as React.ReactElement;
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "register",
