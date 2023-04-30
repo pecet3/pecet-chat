2297cd4b83715d0796696b49b786e7dd2bc6d0da
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-30 12:42:53 +0200

    removed chatContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChatContextProvider } from "./ChatContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChatContextProvider>
-      <App />
-    </ChatContextProvider>
+    <App />
   </React.StrictMode>
 );
