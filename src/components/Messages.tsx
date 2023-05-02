de596969cce150889201048297f3974a4fc3cfcb
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-02 17:02:15 +0200

    edited unsub

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import Message from "./Message";
-import { onSnapshot, doc } from "firebase/firestore";
+import { onSnapshot, doc, DocumentData } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import ChatContext, { IChatContext } from "../context/ChatContext";
 
 const Messages: React.FC = () => {
   const { state } = React.useContext(ChatContext) as IChatContext;
-  const [messages, setMessages] = React.useState([]);
+  const [messages, setMessages] = React.useState<DocumentData | null>(null);
   React.useEffect(() => {
     const unsub = onSnapshot(doc(db, "chats", state.chatId), (doc) => {
-      if (!doc.exists()) return;
-      setMessages(doc.data().messages);
+      doc.exists() && setMessages(doc.data());
     });
 
     return () => unsub();
-  }, []);
+  }, [state.chatId]);
+
+  React.useEffect(() => {
+    console.log(messages);
+  }, [messages]);
   return (
-    <div className="flex h-[calc(100%-112px)] flex-col gap-1 overflow-y-scroll p-1">
-      <Message />
-    </div>
+    <div className="flex h-[calc(100%-112px)] flex-col gap-1 overflow-y-scroll p-1"></div>
   );
 };
 
