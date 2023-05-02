dcb2948760d8e8f34cd711fc22973471ddeb00bd
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-02 16:08:57 +0200

    implemented onSnapshot

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Message from "./Message";
+import { onSnapshot, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import ChatContext, { IChatContext } from "../context/ChatContext";
 
 const Messages: React.FC = () => {
+  const { state } = React.useContext(ChatContext) as IChatContext;
+  const [messages, setMessages] = React.useState([]);
+  React.useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", state.chatId), (doc) => {
+      if (!doc.exists()) return;
+      setMessages(doc.data().messages);
+    });
+
+    return () => unsub();
+  }, []);
   return (
     <div className="flex h-[calc(100%-112px)] flex-col gap-1 overflow-y-scroll p-1">
-      <Message /> <Message /> <Message /> <Message />
+      <Message />
     </div>
   );
 };
