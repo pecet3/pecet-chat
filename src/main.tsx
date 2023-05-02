93c83fd34df18387ea57ce93cbda3d55930844a4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-02 14:31:06 +0200

    impleented chatContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { ChatContextProvider } from "./context/ChatContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
