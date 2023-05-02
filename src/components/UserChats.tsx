f7bea0f60902c321d259fb013e774deb6b7d8426
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-02 11:46:39 +0200

    implemented onSnapshot

--- src/components/UserChats.tsx
+++ src/components/UserChats.tsx
@@ -1,17 +1,44 @@
 import React from "react";
-
+import { doc, onSnapshot, DocumentData } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import Context, { IContext } from "../AuthContext";
 const UserChats: React.FC = () => {
+  const [chats, setChats] = React.useState<DocumentData | undefined>([]);
+
+  const { user } = React.useContext(Context) as IContext;
+
+  React.useEffect(() => {
+    if (!user) return;
+    const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+      setChats(doc.data());
+    });
+    return () => unsub();
+  }, [user?.uid]);
+
+  React.useEffect(() => {
+    if (chats) console.log(Object.entries(chats));
+  }, [chats]);
   return (
-    <div className="flex items-center gap-1 text-left duration-200 hover:bg-slate-600">
-      <img
-        src="https://d-art.ppstatic.pl/kadry/k/r/1/57/8f/6290b1787d225_o_large.jpg"
-        className="ml-1 h-10 w-10 rounded-full object-cover"
-      />
-      <span>
-        <p className="font-bold text-slate-200 ">Agnieszka</p>
-        <p>Lorem ipsum dolor sit.</p>
-      </span>
-    </div>
+    <>
+      {chats &&
+        Object.entries(chats).map((chat) => (
+          <div
+            key={chat[0]}
+            className="flex items-center gap-1 text-left duration-200 hover:bg-slate-600"
+          >
+            <img
+              src={chat[1].userInfo.photoURL}
+              className="ml-1 h-10 w-10 rounded-full object-cover"
+            />
+            <span>
+              <p className="font-bold text-slate-200 ">
+                {chat[1].userInfo.displayName}
+              </p>
+              <p>Lorem ipsum dolor sit.</p>
+            </span>
+          </div>
+        ))}
+    </>
   );
 };
 
