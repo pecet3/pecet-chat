289e87be8ae016cfa97b460a73064af70474e69a
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-02 14:19:13 +0200

    created ChatContext

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,32 +1,54 @@
 import React from "react";
+import AuthContext, { IAuthContext } from "./AuthContext";
 
 interface IProvider {
   children: React.ReactNode;
 }
 
-export interface IChatContext {}
+interface IChatState {
+  chatId: string;
+  user: Object;
+}
+
+interface IUser {
+  uid: string;
+}
+
+type TChatAction = { type: "CHANGE_USER"; payload: IUser };
+
+export interface IChatContext {
+  state: IChatState;
+  dispatch: React.Dispatch<TChatAction>;
+}
 export const ChatContextProvider: React.FC<IProvider> = ({ children }) => {
-const {user} = React.useContext(AuthContext) as IAuthContext
-  const INITIAL_STATE = {
-    chatId: "null",
+  const { user } = React.useContext(AuthContext) as IAuthContext;
+  const INITIAL_STATE: IChatState = {
+    chatId: "",
     user: {},
   };
-  const chatReducer = (state, action) => {
+  const chatReducer = (state: IChatState, action: TChatAction) => {
+    if (!user || !action.payload) return state;
     switch (action.type) {
       case "CHANGE_USER":
         return {
-            chatId: user.uid > findedUser.uid
-            ? user.uid + findedUser.uid
-            : findedUser.uid + user.uid;,
+          chatId:
+            user.uid > action.payload.uid
+              ? user.uid + action.payload.uid
+              : action.payload.uid + user.uid,
           user: action.payload,
         };
       default:
         return state;
     }
   };
-  return <Context.Provider value={{}}>{children}</Context.Provider>;
+  const [state, dispatch] = React.useReducer<
+    React.Reducer<IChatState, TChatAction>
+  >(chatReducer, INITIAL_STATE);
+  return (
+    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+  );
 };
 
-const Context = React.createContext<IAuthContext | null>(null);
+const Context = React.createContext<IChatContext | null>(null);
 
 export default Context;
