ded9f3c53405f9ddd4acabbe3b076056502f5f67
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-03 12:37:20 +0200

    added p to jsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ const Header: React.FC = () => {
   return (
     <header className="my-4">
       <h1 className="text-3xl">pecetChat</h1>
+      <p className="ml-12 text-xs">private chat for you</p>
     </header>
   );
 };
