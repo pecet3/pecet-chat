7a6a3d39b474bd0eebee0ccfb13335e11ac1a13d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-03 22:20:42 +0200

    updated tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header: React.FC = () => {
   return (
     <header className="my-4">
       <h1 className="text-3xl">pecetChat</h1>
-      <p className="ml-12 text-xs">private chat for you</p>
+      <p className=" text-xs">a private chat for you</p>
     </header>
   );
 };
