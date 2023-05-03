19c15da125df85f2e4b3332e951738403a0ddb72
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-03 22:45:51 +0200

    updated tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
+import { TbMessageCircle } from "react-icons/tb";
 const Header: React.FC = () => {
   return (
-    <header className="my-4">
-      <h1 className="text-3xl">pecetChat</h1>
-      <p className=" text-xs">a private chat for you</p>
+    <header className="m-auto my-4 flex justify-center">
+      <h1 className="ml-6 text-3xl">pecetChat</h1>
+      <TbMessageCircle size="24" className="self-start text-slate-700" />
     </header>
   );
 };
