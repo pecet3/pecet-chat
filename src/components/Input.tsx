cbd540127af0ef684081647fc8c94922eef3caf5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-05 10:11:00 +0200

    added useState, created changehandler

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,22 +3,37 @@ import { BiImageAdd } from "react-icons/bi";
 import { MdAttachFile } from "react-icons/md";
 
 const Input: React.FC = () => {
+  const [input, setInput] = React.useState({
+    message: "",
+    file: null,
+  });
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const eventElement = e.target as HTMLInputElement;
+  };
   return (
-    <div className=" flex gap-1 bg-gray-300 p-1">
+    <form className=" flex gap-1 bg-gray-300 p-1">
       <input
         type="text"
         placeholder="enter your message"
         className="w-full rounded-md p-1 text-left"
+        value={input.message}
+        onChange={changeHandler}
       />
       <span className="flex items-center">
         <BiImageAdd size="28" />
-        <input type="file" id="file" className="hidden" />
+        <input
+          type="file"
+          id="file"
+          className="hidden"
+          onChange={changeHandler}
+        />
         <label htmlFor="file">
           <MdAttachFile size="28" />
         </label>
       </span>
       <button className="submitButton">Send</button>
-    </div>
+    </form>
   );
 };
 
