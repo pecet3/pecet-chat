5e810e7376428b36ed18eacb799710e3b718ad1e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-05 10:19:17 +0200

    created TInput, edited changeHandler

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,19 +2,35 @@ import React from "react";
 import { BiImageAdd } from "react-icons/bi";
 import { MdAttachFile } from "react-icons/md";
 
+export type TInput = {
+  message: string;
+  file: null | File;
+};
+
 const Input: React.FC = () => {
-  const [input, setInput] = React.useState({
+  const [input, setInput] = React.useState<TInput>({
     message: "",
     file: null,
   });
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const eventElement = e.target as HTMLInputElement;
+    const inputElement = e.target as HTMLInputElement;
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    if (inputElement.files === null) return;
+    setInput({
+      ...input,
+      file: inputElement.files[0],
+    });
   };
   return (
     <form className=" flex gap-1 bg-gray-300 p-1">
       <input
         type="text"
+        name="message"
         placeholder="enter your message"
         className="w-full rounded-md p-1 text-left"
         value={input.message}
