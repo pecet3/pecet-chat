16a748568739ec91c7bb1201ee53bf6bee1a5d32
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-05 14:32:29 +0200

    added requirment to return special jsx for sender

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatContext, { IChatContext } from "../context/ChatContext";
+import AuthContext, { IAuthContext } from "../context/AuthContext";
 import { DocumentData } from "firebase/firestore";
 
 interface IMessage {
@@ -8,31 +9,65 @@ interface IMessage {
 
 const Message: React.FC<IMessage> = ({ message }) => {
   const { state } = React.useContext(ChatContext) as IChatContext;
+  const { user } = React.useContext(AuthContext) as IAuthContext;
 
-  return (
-    <div className="flex gap-1 text-slate-200">
-      <span>
-        <img
-          src={state.user && state.user.photoURL}
-          alt="user image"
-          className="m-auto h-8 w-8 rounded-full object-fill"
-        />
-        <p>Just now</p>
-      </span>
-      <span className="flex flex-col gap-1">
-        <p className="flex flex-col justify-center rounded-b-lg rounded-r-lg bg-slate-600 p-1 text-left">
-          {message && message.text}
-        </p>
-        {message.img && (
+  if (user?.uid === message.senderId) {
+    return (
+      <div className="flex flex-row-reverse gap-1 text-slate-200">
+        <span>
           <img
-            src={message.img ? message.img : ""}
-            alt="photo which user sent"
-            className="max-h-48 self-start rounded-sm"
+            src={user?.photoURL || ""}
+            alt="user's image"
+            className="m-auto h-8 w-8 rounded-full object-cover"
           />
-        )}
-      </span>
-    </div>
-  );
+          <p>Just now</p>
+        </span>
+        <span className="flex flex-col gap-1">
+          {message && message.text !== "" && (
+            <p className="flex flex-col justify-center rounded-b-lg rounded-l-lg bg-zinc-600 p-1 text-left">
+              {message.text}
+            </p>
+          )}
+
+          {message.img && (
+            <img
+              src={message.img || ""}
+              alt="photo which user sent"
+              className="max-h-48 self-start rounded-sm"
+            />
+          )}
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex gap-1 text-slate-200">
+        <span>
+          <img
+            src={state.user.photoURL || ""}
+            alt="user's image"
+            className="m-auto h-8 w-8 rounded-full object-cover"
+          />
+          <p>Just now</p>
+        </span>
+        <span className="flex flex-col gap-1">
+          {message && message.text !== "" && (
+            <p className="flex flex-col justify-center rounded-b-lg rounded-r-lg bg-slate-600 p-1 text-left">
+              {message.text}
+            </p>
+          )}
+
+          {message.img && (
+            <img
+              src={message.img || ""}
+              alt="photo which user sent"
+              className="max-h-48 self-start rounded-sm"
+            />
+          )}
+        </span>
+      </div>
+    );
+  }
 };
 
 export default Message;
