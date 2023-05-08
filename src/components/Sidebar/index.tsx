bb0b105f166cfadd202cd72d29806e87677bc1d9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-09 00:16:37 +0200

    implemented PublicRooms

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import UserChats from "./UserChats";
-
+import PublicRooms from "./PublicRooms";
 const Sidebar: React.FC = () => {
   return (
     <div className="basis-1/3 bg-slate-500">
       <Navbar />
       <Search />
       <UserChats />
+      <PublicRooms />
     </div>
   );
 };
