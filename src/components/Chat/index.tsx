8476945befecdc7ad6b0f022d4164ca7c446e7c4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-10 00:45:58 +0200

    updated tailwind

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -7,9 +7,19 @@ import Input from "./Input";
 const Chat: React.FC = () => {
   const { state } = React.useContext(ChatContext) as IChatContext;
   return (
-    <div className="basis-2/3 flex-col justify-between bg-slate-400">
-      <div className="flex justify-between bg-slate-500 px-2 py-5 text-slate-200">
-        <p>{state.isPublic ? state.room : state.user.displayName}</p>
+    <div
+      className={`basis-2/3 flex-col justify-between ${
+        state.isPublic ? "bg-zinc-400 " : "bg-slate-400 "
+      }`}
+    >
+      <div
+        className={`flex justify-between ${
+          state.isPublic ? "bg-zinc-100 text-black" : "bg-slate-500 "
+        } px-2 py-5 text-slate-200`}
+      >
+        <p className="font-bold">
+          {state.isPublic ? state.room : state.user.displayName}
+        </p>
         <span className="flex gap-2">
           <BsCameraVideo size="24" />
           <BsThreeDots size="24" />
