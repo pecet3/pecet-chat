1a20edfc875f5bd89e6791ecf74f6715e745f5e3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-05-11 20:04:57 +0200

    updated tailwind, added link to editProfile

--- src/components/Sidebar/Navbar.tsx
+++ src/components/Sidebar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
 import Context, { IAuthContext } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 import { TbMessageCircle } from "react-icons/tb";
 const cookies = new Cookies();
@@ -19,11 +19,11 @@ const Navbar: React.FC = () => {
   return (
     <>
       <nav className="flex flex-col bg-slate-700 p-2 text-gray-200">
-        <span className="flex justify-center">
+        <div className="flex justify-center">
           <h1 className="font-bold">pecetChat</h1>
           <TbMessageCircle size="18" className="ml-[2px]" />
-        </span>
-        <span className="flex justify-between">
+        </div>
+        <div className="flex justify-between">
           {user && (
             <span className="flex gap-1">
               <img
@@ -33,14 +33,18 @@ const Navbar: React.FC = () => {
               <p>{user.displayName}</p>
             </span>
           )}
-
-          <button
-            className="rounded-md bg-slate-300 px-1 text-xs text-slate-900 transition-all duration-200 hover:rounded-lg"
-            onClick={handleLogout}
-          >
-            Log Out
-          </button>
-        </span>
+          <span className="flex gap-1">
+            <button className="rounded-md bg-blue-300 px-1 text-xs text-slate-900 transition-all duration-200 hover:rounded-lg">
+              <Link to="/editProfile">Edit a Profile</Link>
+            </button>
+            <button
+              className="justify- rounded-md bg-slate-300 px-1 text-xs text-slate-900 transition-all duration-200 hover:rounded-lg"
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
+          </span>
+        </div>
       </nav>
     </>
   );
