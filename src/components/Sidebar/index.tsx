262236e62a382a2f7dc4efa4495db8bba31e79df
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-13 01:03:26 +0200

    implemented state from chatcontecxt

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,7 +3,12 @@ import Navbar from "./Navbar";
 import Search from "./Search";
 import UserChats from "./UserChats";
 import PublicRooms from "./PublicRooms";
+import ChatContext, { IChatContext } from "../../context/ChatContext";
+
 const Sidebar: React.FC = () => {
+  const { state } = React.useContext(ChatContext) as IChatContext;
+
+  const isSidebar = state.isSidebar;
   return (
     <div className="basis-1/3 bg-slate-500">
       <Navbar />
