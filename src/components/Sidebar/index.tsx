7ff20bcac1a2be02581bdb6f89cede1f36b9bd1a
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-13 22:55:13 +0200

    upadted tailwid // fixed sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,19 +3,25 @@ import Navbar from "./Navbar";
 import Search from "./Search";
 import UserChats from "./UserChats";
 import PublicRooms from "./PublicRooms";
+
 import ChatContext, { IChatContext } from "../../context/ChatContext";
 
-const Sidebar: React.FC = () => {
+const Sidebar: React.FC = ({}) => {
   const { state } = React.useContext(ChatContext) as IChatContext;
 
   const isSidebar = state.isSidebar;
+
   return (
-    <div className="basis-1/3 bg-slate-500">
+    <aside
+      className={` bg-slate-500 ${
+        isSidebar ? "" : "hidden md:block"
+      } md:basis-1/3`}
+    >
       <Navbar />
       <PublicRooms />
       <Search />
       <UserChats />
-    </div>
+    </aside>
   );
 };
 
