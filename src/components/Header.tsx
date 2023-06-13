63d6bf60dfdd222409a7493e2aa218199b054c8b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-13 23:07:57 +0200

    added info about version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ const Header: React.FC = () => {
     <header className="m-auto my-4 flex justify-center">
       <h1 className="ml-6 text-3xl">pecetChat</h1>
       <TbMessageCircle size="24" className="self-start text-slate-700" />
+      <h2 className="text-sm">beta 1.0</h2>
     </header>
   );
 };
