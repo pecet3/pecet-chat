30de1bab9fe2ddb592f99678a3c32e4b265c48e1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-14 00:04:00 +0200

    created custom hook

---
+++ src/helpers/useGetWidth.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+
+export const useGetWidth = () => {
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setInnerWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return innerWidth as number;
+}
