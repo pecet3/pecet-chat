e1fd3b6e03e3c871a866e3bbfccf2be9ad0483ce
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-15 00:04:36 +0200

    updated tailwind for responsive // almost ready

--- src/components/Sidebar/UserChats.tsx
+++ src/components/Sidebar/UserChats.tsx
@@ -34,9 +34,9 @@ const UserChats: React.FC = () => {
         Object.entries(chats)
           .sort((a, b) => b[1].date - a[1].date)
           .map((chat) => (
-            <button
+            <li
               key={chat[0]}
-              className="flex items-center gap-1 object-contain py-1 text-left duration-200 hover:bg-slate-600"
+              className="flex flex-1 gap-1 p-1 text-left duration-200 hover:cursor-pointer hover:bg-slate-600"
               onClick={() => handleSelect(chat[1].userInfo)}
             >
               <img
@@ -45,7 +45,7 @@ const UserChats: React.FC = () => {
                     ? chat[1].userInfo.photoURL
                     : ""
                 }
-                className="ml-1 h-10 w-10 rounded-full object-cover"
+                className="h-10 w-10 rounded-full object-cover"
               />
               <span>
                 <p className="font-bold text-white ">
@@ -55,13 +55,13 @@ const UserChats: React.FC = () => {
                   <p className="text-sm italic text-slate-300">photo</p>
                 ) : (
                   chat[1].lastMessage && (
-                    <p className="w-64 truncate text-sm italic text-slate-300">
+                    <p className="truncate text-sm italic text-slate-300">
                       {chat[1].lastMessage}
                     </p>
                   )
                 )}
               </span>
-            </button>
+            </li>
           ))}
     </>
   );
