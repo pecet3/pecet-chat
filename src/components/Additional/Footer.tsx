863dab400188a6585b269c68e6435ecb5d829ccd
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-15 23:26:40 +0200

    updated tailwind

--- src/components/Additional/Footer.tsx
+++ src/components/Additional/Footer.tsx
@@ -1,8 +1,14 @@
+import { useSize } from "../../helpers/useSize";
 export const Footer = () => {
+  const { innerHeight } = useSize();
   return (
-    <footer className="m-8 font-extralight text-slate-600">
+    <footer
+      className={`mt-2 font-extralight text-slate-600 md:mt-8 ${
+        innerHeight < 670 && "hidden"
+      }`}
+    >
       developed with ❤ by <b>Jakub Pacewicz</b>, <br />
-      <p className="m-2 text-xs">version: beta 1.3</p>
+      <p className="text-xs">version: beta 1.8</p>
     </footer>
   );
 };
