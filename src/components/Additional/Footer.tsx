e3a118db75aa59b8e649a7d3deaf89a1f40b9af1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-06-15 22:08:25 +0200

    created the component

---
+++ src/components/Additional/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className="m-8 font-extralight text-slate-600">
+      developed with ❤ by <b>Jakub Pacewicz</b>, <br />
+      <p className="m-2 text-xs">version: beta 1.3</p>
+    </footer>
+  );
+};
